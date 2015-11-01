39310df373ab173b31034e120a48eca1a0c5a7cc
Saurabh Vishwas Joshi <sjoshi6@ncsu.edu>
2015-11-01 13:59:54 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,12 @@
+
+# Esper API Server
+
+## Instructions to run esper server on ubuntu
+```
 sudo apt-get install npm -y
 sudo apt-get install nodejs-legacy -y
 ln -s /usr/bin/nodejs /usr/bin/node
 
 npm install
-node app.js
+nohup node app.js &
+```
